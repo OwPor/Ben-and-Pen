1afae0917bc7fb79de23d9f5e2e196a5abb85ab3
Ray Vincent DS. Concepcion <rayvincentconcepcion@gmail.com>
2024-12-25 21:52:37 +0800

    Add files via upload

---
+++ Player2.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+const SPEED = 140.0
+const JUMP_VELOCITY = -525.0
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+var isLeft = false
+
+func _physics_process(delta):
+	if abs(velocity.x) > 1:
+		$AnimatedSprite2D.animation = "running"
+	else:
+		$AnimatedSprite2D.animation = "idle"
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	if Input.is_action_just_pressed("ui_up") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+		$AnimatedSprite2D.animation = "jumping"
+	
+	if Input.is_action_pressed("ui_left"):
+		isLeft = true
+	elif Input.is_action_pressed("ui_right"):
+		isLeft = false
+		
+	var direction = Input.get_axis("ui_left", "ui_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 10)
+
+	move_and_slide()
+	
+	$AnimatedSprite2D.flip_h = isLeft
